5470ee9ecf7c460566293ed3a813f7eea9dc95e2
anastazoch <anastas_zoch@yahoo.gr>
2017-03-23 09:48:34 +0200

    Update virtuozzo_license_update.sh

--- virtuozzo_license_update.sh
+++ virtuozzo_license_update.sh
@@ -3,11 +3,10 @@
 HOSTNAME=`hostname | awk '{print toupper($0)}'`
 UPDATE_DATE=`sudo vzlicview | grep update | awk -F = '{print $2}' | awk -F '"' '{print $2}' | awk -F ' ' '{print $1}'`
 EXPIRATION_DATE=`sudo vzlicview | grep expiration | awk -F '=' '{print $2}' | awk -F '"' '{print $2}' | awk -F ' ' '{print $1}'`
-DAYS_REMAINING=`echo $(( ($(date --date=$EXPIRATION_DATE +%s) - $(date +%s))/(24*60*60) ))`
+DAYS_LEFT=`echo $(( ($(date --date=$EXPIRATION_DATE +%s) - $(date +%s))/(24*60*60) ))`
 
-if (( $(date +%s)/(24*60*60) > $(date --date=$UPDATE_DATE +%s)/(24*60*60) ))
-then
-	printf "Η ενημέρωση της άδειας Virtuozzo του συστήματος %s απέτυχε.\n\n \ 
-	Απομένουν %s ημέρες για λήξη της άδειας.\n\n \
-	Επικοινωνήστε με Virtuozzo.\n" $HOSTNAME_TO_UPPERCASE $DAYS_REMAINING | mail -s "Ενημέρωση άδειας Virtuozzo" itcs@ote.gr
+if (( $(date +%s)/(24*60*60) > $(date --date=$UPDATE_DATE +%s)/(24*60*60) )); then
+	printf "License update in the system %s has failed.\n\n \ 
+	%s days left before your license expires.\n\n \
+	Please contact Virtuozzo.\n" $HOSTNAME_TO_UPPERCASE $DAYS_LEFT | mail -s "Ενημέρωση άδειας Virtuozzo" itcs@ote.gr
 fi
