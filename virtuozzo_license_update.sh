8a14ea6d9895fd2bb0f5bc513681af518ce56e9c
anastazoch <anastas_zoch@yahoo.gr>
2017-11-11 20:08:29 +0200

    Update virtuozzo_license_update.sh

--- virtuozzo_license_update.sh
+++ virtuozzo_license_update.sh
@@ -8,8 +8,8 @@ DAYS_LEFT=`echo $(( ($(date --date=$EXPIRATION_DATE +%s) - $(date +%s))/(24*60*6
 if (( $(date +%s)/(24*60*60) > $(date --date=$UPDATE_DATE +%s)/(24*60*60) )); then
 	if (( $(date +%s)/(24*60*60) < $(date --date=$EXPIRATION_DATE +%s)/(24*60*60) )); then
 		printf "License update in the system %s has failed.\n\n \ 
-		%s days left before Virtuozzo license expiration.\n\n \
-		Please contact Virtuozzo.\n" $HOSTNAME $DAYS_LEFT | mail -s "Ενημέρωση άδειας Virtuozzo" itcs@ote.gr
+			%s days left before Virtuozzo license expiration.\n\n \
+			Please contact Virtuozzo.\n" $HOSTNAME $DAYS_LEFT | mail -s "Ενημέρωση άδειας Virtuozzo" itcs@ote.gr
 	elif (( $(date +%s)/(24*60*60) > $(date --date=$EXPIRATION_DATE +%s)/(24*60*60) )); then
 		printf "Virtuozzo license in the system %s has expired.\n\n \
 		Please contact Virtuozzo.\n" $HOSTNAME $DAYS_LEFT | mail -s "Ενημέρωση άδειας Virtuozzo" itcs@ote.gr
