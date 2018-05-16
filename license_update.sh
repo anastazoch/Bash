ff0ac6b9964ffd8daea9e6f746aa9187e5d3098b
anastazoch <anastas_zoch@yahoo.gr>
2018-05-16 11:00:35 +0300

    Update and rename virtuozzo_license_update.sh to license_update.sh

--- license_update.sh
+++ license_update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+MAIL_ADDRESS=...
 HOSTNAME=`hostname | awk '{print toupper($0)}'`
 UPDATE_DATE=`sudo vzlicview | grep update | awk -F = '{print $2}' | awk -F '"' '{print $2}' | awk -F ' ' '{print $1}'`
 EXPIRATION_DATE=`sudo vzlicview | grep expiration | awk -F '=' '{print $2}' | awk -F '"' '{print $2}' | awk -F ' ' '{print $1}'`
@@ -7,11 +8,11 @@ DAYS_LEFT=`echo $(( ($(date --date=$EXPIRATION_DATE +%s) - $(date +%s))/(24*60*6
 
 if (( $(date +%s)/(24*60*60) > $(date --date=$UPDATE_DATE +%s)/(24*60*60) )); then
 	if (( $(date +%s)/(24*60*60) < $(date --date=$EXPIRATION_DATE +%s)/(24*60*60) )); then
-		printf "License update in the system %s has failed.\n\n \ 
-			%s days left before Virtuozzo license expiration.\n\n \
-			Please contact Virtuozzo.\n" $HOSTNAME $DAYS_LEFT | mail -s "Ενημέρωση άδειας Virtuozzo" itcs@ote.gr
+		printf "System's license %s has failed.\n\n \ 
+			%s days left before license expiration.\n\n \
+			Please contact the ISV.\n" $HOSTNAME $DAYS_LEFT | mail -s "License update" MAIL_ADDRESS
 	elif (( $(date +%s)/(24*60*60) > $(date --date=$EXPIRATION_DATE +%s)/(24*60*60) )); then
-		printf "Virtuozzo license in the system %s has expired.\n\n \
-		Please contact Virtuozzo.\n" $HOSTNAME $DAYS_LEFT | mail -s "Ενημέρωση άδειας Virtuozzo" itcs@ote.gr
+		printf "System's %s license has expired.\n\n \
+		Please contact the ISV.\n" $HOSTNAME $DAYS_LEFT | mail -s "License update" MAIL_ADDRESS
 	fi		
 fi
